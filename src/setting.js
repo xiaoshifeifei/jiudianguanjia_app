1e166d2e18495b7c07b4e0eacaa169191b6f4712
98dang <250455118@qq.com>
2020-06-29 18:05:39 +0800

    修改成不跳授权页面

--- src/setting.js
+++ src/setting.js
@@ -1,5 +1,5 @@
 module.exports = {
-  authUrl: 'http://b.capasky.com/mobile/wechat/auth', // process.env.VUE_APP_AUTH_URL + '/mobile/wechat/auth',
+  authUrl: process.env.VUE_APP_AUTH_URL + '/mobile/wechat/auth',
   appid: 'wx48bc815394dc5ca9',
   // eslint-disable-next-line camelcase
   token_type: 'Bearer'
