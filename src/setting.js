14bc119f4bbc57d91aff610adf7b25639f6c0915
GaryHuang <19020986@qq.com>
2020-07-22 17:16:36 +0800

    环境配置更新

--- src/setting.js
+++ src/setting.js
@@ -1,6 +1,6 @@
 module.exports = {
   authUrl: process.env.VUE_APP_AUTH_URL + '/mobile/wechat/auth',
-  appid: process.env.WECHAT_APPID,
+  appid: process.env.VUE_APP_WECHAT_APPID,
   // eslint-disable-next-line camelcase
   token_type: 'Bearer'
 }
